78abced6596ecc247d94104ea87394d2e8ed6cee
DenisArdi <91360331+DenisArdi@users.noreply.github.com>
2021-09-25 17:25:55 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama Saya Denis Ardi Saputra
+            Saya tinggal di daerah Bekasi
+            Olahraga yang saya sukai adalah Badminton";
+    }
+}
